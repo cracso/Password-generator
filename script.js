320b930b6ca5ba68a08dad4527ec16f905d79964
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2020-07-23 18:58:52 -0400

    added

---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lengthEl = document.querySelector("length")
+
+ 
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  function generatePassword() {
+    var length = "20"
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+<>"
+        retVal = "";
+    
+        for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+
+
+
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
